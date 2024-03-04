b17879d2727f4ddff098a8432f93a580ae16de1f
SebastianLandaeta <sebas.landa@hotmail.com>
2024-03-04 19:49:14 -0400

    Final
    Proyecto "final"

--- scanner_pn/lib/main.dart
+++ scanner_pn/lib/main.dart
@@ -1,3 +1,6 @@
+// Importación de librerías
+// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api, use_key_in_widget_constructors
+
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -6,14 +9,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
+// Función main
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized(); // Inicializar aplicación justo a los servicios de Firebase
   await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
 }
 
+// Widget estático de la aplicación
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -27,6 +32,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// Widgets dinámicos de la aplicación
 class ImagePickerPage extends StatefulWidget {
   @override
   _ImagePickerPageState createState() => _ImagePickerPageState();
@@ -43,109 +49,105 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
 
     setState(() {
       _imageFile = File(pickedFile!.path);
-      _extractedText = ''; // Reset extracted text when a new image is picked
+      _extractedText = ''; // Se vacía la variable en caso de que se quiera seleccionar otra imagen
     });
 
     _extractTextFromImage();
   }
+  
+  // Extraemos el texto de la imagen
+  Future<void> _extractTextFromImage() async {
+    if (_imageFile == null) return;
 
-Future<void> _extractTextFromImage() async {
-  if (_imageFile == null) return;
+    final inputImage = InputImage.fromFile(_imageFile!);
+    final textRecognizer = TextRecognizer();
+    final text = await textRecognizer.processImage(inputImage);
 
-  final inputImage = InputImage.fromFile(_imageFile!);
-  final textRecognizer = TextRecognizer();
-  final text = await textRecognizer.processImage(inputImage);
-
-  setState(() {
-    _extractedText = text.text;
-  });
-
-  await textRecognizer.close();
-
-  // Comparar el texto extraído con documentos en Firestore
-  await _compareWithFirestore(text.text);
-}
-
-Future<void> _compareWithFirestore(String extractedText) async {
-  final usersCollection = FirebaseFirestore.instance.collection('users');
-  final querySnapshot = await usersCollection.get();
-
-  for (QueryDocumentSnapshot document in querySnapshot.docs) {
-    final cedula = document['Cédula'];
-    final nombre = document['Nombre'];
+    setState(() {
+      _extractedText = text.text;
+    });
     
-    print('$cedula');
-    print('$nombre');
-    print('$extractedText');
+    // Se cierra la entrada de imagen
+    await textRecognizer.close();
 
-    if (extractedText.trim() == 'Cédula: $cedula\nNombre: $nombre'.trim()) {
-      // Coincidencia encontrada
-      _showConfirmationMessage();
-      return;
-    }
+    // Comparar el texto extraído con documentos en Firestore
+    await _compareWithFirestore(text.text);
   }
 
-  // No se encontró ninguna coincidencia
-  _showNoConfirmationMessage();
-}
+  Future<void> _compareWithFirestore(String extractedText) async {
+    final usersCollection = FirebaseFirestore.instance.collection('users');
+    final querySnapshot = await usersCollection.get();
+  
+    // Se buscará en todos los documentos de la colección si hay alguna coincidencia
+    for (QueryDocumentSnapshot document in querySnapshot.docs) {
+      final cedula = document['Cédula'];
+      final nombre = document['Nombre'];
+    
+      if (extractedText.trim() == 'Cédula: $cedula\nNombre: $nombre'.trim()) {
+        // Coincidencia encontrada
+        _showConfirmationMessage();
+        return;
+      }
+    }
 
-void _showConfirmationMessage() {
-  // Muestra el mensaje de confirmación
-  // Puedes usar showDialog, SnackBar, o cualquier otro método que prefieras
-  // Ejemplo:
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: Text('Confirmación'),
-      content: Text('El texto coincide con un usuario en la base de datos.'),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('OK'),
-        ),
-      ],
-    ),
-  );
-}
+    // No se encontró ninguna coincidencia
+    _showNoConfirmationMessage();
+  }
 
-void _showNoConfirmationMessage() {
-  // Muestra el mensaje de no confirmación
-  // Ejemplo:
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: Text('No Confirmación'),
-      content: Text('El texto no coincide con ningún usuario en la base de datos.'),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('OK'),
-        ),
-      ],
-    ),
-  );
-}
+  void _showConfirmationMessage() {
+    // Muestra el mensaje de confirmación
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Confirmación'),
+        content: Text('El texto coincide con un usuario en la base de datos.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Muestra el mensaje de error
+  void _showNoConfirmationMessage() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Error'),
+        content: Text('La información no coincide con ningún usuario de la base de datos.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Text Recognition Example'),
+        title: Text('Scanner de Partidas de Nacimiento'),
       ),
       body: Center(
         child: _imageFile == null
-            ? Text('No image selected')
+            ? Text('No ha seleccionado ninguna imagen')
             : Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Image.file(_imageFile!),
                   SizedBox(height: 20),
                   Text(
-                    'Extracted Text:',
+                    'Texto Extraído:',
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(height: 10),
@@ -165,13 +167,13 @@ void _showNoConfirmationMessage() {
         children: [
           FloatingActionButton(
             onPressed: () => _pickImage(ImageSource.camera),
-            tooltip: 'Take Photo',
+            tooltip: 'Tomar Foto',
             child: Icon(Icons.camera),
           ),
           SizedBox(height: 16),
           FloatingActionButton(
             onPressed: () => _pickImage(ImageSource.gallery),
-            tooltip: 'Select Image',
+            tooltip: 'Seleccionar Imagen',
             child: Icon(Icons.photo_library),
           ),
         ],
